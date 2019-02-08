a2dfaf2b117c284cb25ac5ceb1a3e8b4e5424fae
Tiago Osvald Ramos <tiagoor@gmail.com>
2019-02-08 12:39:08 -0200

    Create install.htop.sh

---
+++ install/install.htop.sh
@@ -0,0 +1,6 @@
+# -------------- For RHEL/CentOS 7 --------------
+# version 0
+# 
+wget dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
+# 
+rpm -ihv epel-release-7-11.noarch.rpm 
